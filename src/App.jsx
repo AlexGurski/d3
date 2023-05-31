6023eea8bbc69ca17e9556dc94d345e1c3e2c0c5
etosolo322 <etosolo322@yandex.ru>
2023-05-31 15:22:32 +0300

    0.1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,146 @@
 import VerticalTimeline from "./VerticalTimeline";
+// const data = [
+//   {
+//     indeks: 1306813,
+//     date: "2023-05-26T14:43:39.290000Z",
+//     stanowisko: 25,
+//     uzytkownik: 35,
+//     raport: "OkuwanieRam",
+//     worker: "Artūras Brazauskas",
+//     status: null,
+//     video_data: {
+//       status: false,
+//     },
+//   },
+//   {
+//     indeks: 1306815,
+//     date: "2023-05-26T14:44:10.913000Z",
+//     stanowisko: 25,
+//     uzytkownik: 35,
+//     raport: "OkuwanieRam",
+//     worker: "Artūras Brazauskas",
+//     status: null,
+//     video_data: {
+//       status: false,
+//     },
+//   },
+//   {
+//     indeks: 1306826,
+//     date: "2023-05-26T14:47:01.060000Z",
+//     stanowisko: 25,
+//     uzytkownik: 35,
+//     raport: "OkuwanieRam",
+//     worker: "Artūras Brazauskas",
+//     status: null,
+//     video_data: {
+//       status: false,
+//     },
+//   },
+//   {
+//     indeks: 1306828,
+//     date: "2023-05-26T14:47:26.737000Z",
+//     stanowisko: 25,
+//     uzytkownik: 35,
+//     raport: "OkuwanieRam",
+//     worker: "Artūras Brazauskas",
+//     status: null,
+//     video_data: {
+//       status: false,
+//     },
+//   },
+//   {
+//     indeks: 1308277,
+//     date: "2023-05-29T11:30:30.183000Z",
+//     stanowisko: 43,
+//     uzytkownik: 136,
+//     raport: "Komplektavimas",
+//     worker: "Kateryna Poberzhniuk",
+//     status: null,
+//     video_data: {
+//       status: false,
+//     },
+//   },
+//   {
+//     indeks: 1309011,
+//     date: "2023-05-29T15:12:30.877000Z",
+//     stanowisko: 13,
+//     uzytkownik: 111,
+//     raport: "OkuwanieRamRotox",
+//     worker: "Babin Dmitro",
+//     status: null,
+//     video_data: {
+//       status: false,
+//       camera_ip: "10.20.100.40",
+//     },
+//   },
+//   {
+//     indeks: 1309015,
+//     date: "2023-05-29T15:14:49.293000Z",
+//     stanowisko: 19,
+//     uzytkownik: 64,
+//     raport: "OkuwanieRamKaustymasStandartas",
+//     worker: "Ramūnas Šiugžda",
+//     status: null,
+//     video_data: {
+//       status: false,
+//     },
+//   },
+//   {
+//     indeks: 1309612,
+//     date: "2023-05-30T08:57:11.580000Z",
+//     stanowisko: 4,
+//     uzytkownik: 34,
+//     raport: "StiklinimasMobiliNEWtest",
+//     worker: "Mantas Supronas",
+//     status: null,
+//     video_data: {
+//       status: false,
+//     },
+//   },
+// ];
+
 const data = [
   {
-    indeks: 1306813,
-    date: "2023-05-26T14:43:39.290000Z",
-    stanowisko: 25,
-    uzytkownik: 35,
-    raport: "OkuwanieRam",
-    worker: "Artūras Brazauskas",
-    status: null,
-    video_data: {
-      status: false,
-    },
+    OperationID: 50,
+    OperationName: "Operation_control",
+    operations: [
+      {
+        indeks: 1227966,
+        zlecenieID: 1283619,
+        zlecenie: "G21323",
+        date: "2023-05-27 05:07:22.497",
+      },
+      {
+        indeks: 1227967,
+        zlecenieID: 1283619,
+        zlecenie: "G21323",
+        date: "2023-05-27 09:09:45.290",
+      },
+    ],
   },
   {
-    indeks: 1306815,
-    date: "2023-05-26T14:44:10.913000Z",
-    stanowisko: 25,
-    uzytkownik: 35,
-    raport: "OkuwanieRam",
-    worker: "Artūras Brazauskas",
-    status: null,
-    video_data: {
-      status: false,
-    },
-  },
-  {
-    indeks: 1306826,
-    date: "2023-05-26T14:47:01.060000Z",
-    stanowisko: 25,
-    uzytkownik: 35,
-    raport: "OkuwanieRam",
-    worker: "Artūras Brazauskas",
-    status: null,
-    video_data: {
-      status: false,
-    },
-  },
-  {
-    indeks: 1306828,
-    date: "2023-05-26T14:47:26.737000Z",
-    stanowisko: 25,
-    uzytkownik: 35,
-    raport: "OkuwanieRam",
-    worker: "Artūras Brazauskas",
-    status: null,
-    video_data: {
-      status: false,
-    },
-  },
-  {
-    indeks: 1308277,
-    date: "2023-05-29T11:30:30.183000Z",
-    stanowisko: 43,
-    uzytkownik: 136,
-    raport: "Komplektavimas",
-    worker: "Kateryna Poberzhniuk",
-    status: null,
-    video_data: {
-      status: false,
-    },
-  },
-  {
-    indeks: 1309011,
-    date: "2023-05-29T15:12:30.877000Z",
-    stanowisko: 13,
-    uzytkownik: 111,
-    raport: "OkuwanieRamRotox",
-    worker: "Babin Dmitro",
-    status: null,
-    video_data: {
-      status: false,
-      camera_ip: "10.20.100.40",
-    },
-  },
-  {
-    indeks: 1309015,
-    date: "2023-05-29T15:14:49.293000Z",
-    stanowisko: 19,
-    uzytkownik: 64,
-    raport: "OkuwanieRamKaustymasStandartas",
-    worker: "Ramūnas Šiugžda",
-    status: null,
-    video_data: {
-      status: false,
-    },
-  },
-  {
-    indeks: 1309612,
-    date: "2023-05-30T08:57:11.580000Z",
-    stanowisko: 4,
-    uzytkownik: 34,
-    raport: "StiklinimasMobiliNEWtest",
-    worker: "Mantas Supronas",
-    status: null,
-    video_data: {
-      status: false,
-    },
+    OperationID: 12,
+    OperationName: "Okuwabnnwiodwei",
+    operations: [
+      {
+        indeks: 12,
+        zlecenieID: 128123619,
+        zlecenie: "G213wqw23",
+        date: "2023-05-27 07:07:22.497",
+      },
+      {
+        indeks: 21,
+        zlecenieID: 122,
+        zlecenie: "G21323",
+        date: "2023-05-27 17:09:45.290",
+      },
+      {
+        indeks: 21,
+        zlecenieID: 122,
+        zlecenie: "G21323",
+        date: "2023-05-27 22:09:45.290",
+      },
+    ],
   },
 ];
 export const App = () => {
@@ -103,7 +148,7 @@ export const App = () => {
     <>
       <VerticalTimeline
         data={data}
-        minDate={new Date("2023-05-25T00:00:00.000Z")}
+        minDate={new Date("2023-05-24T00:00:00.000Z")}
         maxDate={new Date("2023-05-30T23:59:59.000Z")}
       />
     </>
--- src/VerticalTimeline.jsx
+++ src/VerticalTimeline.jsx
@@ -1,14 +1,21 @@
 import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
 
+function getDuration(milli) {
+  let minutes = Math.floor(milli / 60000);
+  let hours = Math.round(minutes / 60);
+  let days = Math.round(hours / 24);
+  return days * 400;
+}
+
 const VerticalTimeline = ({ data, minDate, maxDate }) => {
   const timelineRef = useRef(null);
 
   useEffect(() => {
-    const margin = { top: 50, right: 30, bottom: 30, left: 100 };
+    const margin = { top: 50, right: 30, bottom: 30, left: 0 };
     const height =
       timelineRef.current.clientHeight - margin.top - margin.bottom;
-    const width = 200;
+    const width = timelineRef.current.clientWidth;
 
     const svg = d3
       .select(timelineRef.current)
@@ -18,59 +25,83 @@ const VerticalTimeline = ({ data, minDate, maxDate }) => {
       .append("g")
       .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    const parseDate = (e) => new Date(e);
+    const sortedData = (dat) =>
+      dat.sort((a, b) => parseDate(a.date) - parseDate(b.date));
 
-    const sortedData = data.sort(
-      (a, b) => parseDate(a.date) - parseDate(b.date)
-    );
+    const parseDate = (e) => new Date(e);
 
     const y = d3.scaleTime().domain([minDate, maxDate]).range([0, height]);
     const barWidth = 50;
-    const barPadding = 10;
 
-    const bars = svg
-      .selectAll(".timeline-bar")
-      .data(sortedData)
-      .enter()
-      .append("g")
-      .attr("class", "timeline-bar")
-      .attr("transform", (d, i) => `translate(0, ${y(parseDate(d.date))})`)
-      .on("mouseover", function (event, d) {
-        d3.select(this).select("rect").attr("fill", "yellow");
-        console.log(d);
-      })
-      .on("mouseout", function (event, d) {
-        d3.select(this).select("rect").attr("fill", "#87BC45");
-      });
+    data.map((element, index) => {
+      const bars = svg
+        .selectAll(".timeline-bar" + index)
+        .data(() => sortedData(element.operations))
+        .enter()
+        .append("g")
+        .attr("class", "timeline-bar" + index)
+        .attr("transform", (d, i) => `translate(0, ${y(parseDate(d.date))})`)
+        .on("mouseover", function (event, d) {
+          d3.select(this).select("rect").attr("fill", "yellow");
+          console.log(d);
+        })
+        .on("mouseout", function (event, d) {
+          d3.select(this).select("rect").attr("fill", "#87BC45");
+        });
 
-    bars
-      .append("rect")
-      .attr("x", 0)
-      .attr("y", -barWidth / 2)
-      .attr("width", width)
-      .attr("height", (d, i) => {
-        if (i < sortedData.length - 1) {
-          const nextDate = new Date(parseDate(sortedData[i + 1].date) - 1); // Изменение шага времени на 1 час
-          return y(nextDate) - y(parseDate(d.date));
-        }
-        return barWidth;
-      })
-      .attr("fill", "#87BC45");
+      bars
+        .append("rect")
+        .attr("x", index * 200)
+        .attr("y", -barWidth / 2)
+        .attr("width", 90)
+        .attr("height", (d, i) => {
+          if (i < sortedData(element.operations).length - 1) {
+            const nextDate = new Date(parseDate(sortedData(element.operations)[i + 1].date) - 1); // Изменение шага времени на 1 час
+            console.log(nextDate);
+            return y(nextDate) - y(parseDate(d.date));
+          }
+          return barWidth;
+        })
+        .attr("fill", "#87BC45");
+    });
 
     const timeFormat = d3.timeFormat("%d.%m, %H:%M");
     const yAxis = d3
-      .axisLeft(y)
+      .axisRight(y)
       .ticks(d3.timeHour.every(2))
       .tickFormat(timeFormat);
 
-    svg.append("g").attr("class", "y-axis").call(yAxis);
+    svg
+      .append("g")
+      .attr("class", "y-axis")
+      .attr("transform", `translate(${width - 100}, 0)`)
+      .call(yAxis);
 
     return () => {
       d3.select(timelineRef.current).selectAll("*").remove();
     };
   }, [data, minDate, maxDate]);
 
-  return <div ref={timelineRef} style={{ width: "100%", height: "3000px" }} />;
+  return (
+    <>
+      <div
+        ref={timelineRef}
+        style={{ width: "100%", height: `${getDuration(maxDate - minDate)}px` }}
+      />
+      {data.map((element, index) => (
+        <div
+          key={index}
+          style={{
+            position: "absolute",
+            top: "10px",
+            left: `${index * 200}px`,
+          }}
+        >
+          <span>{element.OperationName}</span>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default VerticalTimeline;
